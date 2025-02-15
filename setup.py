21f49ee0ec222672dc9ad11ea8f886ed5678512f
sfgrahman <sfgrahman35@gmail.com>
2025-02-15 12:34:27 +0600

    add cokkiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A complete mlops project, water potability prediction using ML',
+    author='sfgrahman',
+    license='',
+)
